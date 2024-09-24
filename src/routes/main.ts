b769d21a1b30568fbc316f44c7d9a64165254773
wigor <wigor21@hotmail.com>
2024-09-24 18:50:31 -0300

    feat: created routes

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -4,3 +4,24 @@ import * as pingController from "../controllers/ping";
 export const mainRouter = Router();
 
 mainRouter.get("/ping", pingController.ping);
+//mainRouter.get("/privateping");
+
+// mainRouter.post("/auth/signup");
+// mainRouter.post("/auth/signin");
+
+// mainRouter.post("/tweet");
+// mainRouter.get("/tweet/:id");
+// mainRouter.get("/tweet/:id/answers");
+// mainRouter.post("/tweet/:id/like");
+
+// mainRouter.get("/user/:slug");
+// mainRouter.get("/user/:slug/tweets");
+// mainRouter.post("/user/:slug/fallow");
+// mainRouter.put("/user");
+// mainRouter.put("/user/avatar");
+// mainRouter.put("/user/cover");
+
+// mainRouter.get("/feed");
+// mainRouter.get("/search");
+// mainRouter.get("/trending");
+// mainRouter.get("/suggestions");
