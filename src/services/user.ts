bda4aa2933189f830815f61ba38504a380765f59
wigor <wigor21@hotmail.com>
2024-09-24 19:43:35 -0300

    feat: created service to find a user by slug

--- src/services/user.ts
+++ src/services/user.ts
@@ -16,3 +16,25 @@ export const findUserByEmail = async (email: string) => {
 
   return null;
 };
+
+export const findUserBySlug = async (slug: string) => {
+  const user = await prisma.user.findFirst({
+    select: {
+      avatar: true,
+      cover: true,
+      slug: true,
+      name: true,
+      bio: true,
+      link: true,
+    },
+    where: { slug },
+  });
+
+  if (user) {
+    return {
+      ...user,
+      avatar: getPublicUrl(user.avatar),
+      cover: getPublicUrl(user.cover),
+    };
+  }
+};
