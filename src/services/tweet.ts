0da40e0075b5ad2f3def6943ccf13e12a4b36748
wigor <wigor21@hotmail.com>
2024-09-25 13:56:08 -0300

    feat: created service to find a tweet by id

---
+++ src/services/tweet.ts
@@ -0,0 +1,30 @@
+import { prisma } from "../utils/prisma";
+import { getPublicUrl } from "../utils/url";
+
+export const findTweet = async (id: number) => {
+  const tweet = await prisma.tweet.findFirst({
+    include: {
+      user: {
+        select: {
+          name: true,
+          avatar: true,
+          slug: true,
+        },
+      },
+      likes: {
+        select: {
+          userSlug: true,
+        },
+      },
+    },
+    where: { id },
+  });
+
+  if (tweet) {
+    tweet.user.avatar = getPublicUrl(tweet.user.avatar);
+
+    return tweet;
+  }
+
+  return null;
+};
