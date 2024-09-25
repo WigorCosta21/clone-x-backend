175f462a59d259b883d98cbb354a96acc19969f0
wigor <wigor21@hotmail.com>
2024-09-25 13:56:19 -0300

    feat: checking if it is a response

--- src/controllers/tweet.ts
+++ src/controllers/tweet.ts
@@ -1,6 +1,7 @@
 import { Response } from "express";
 import { ExtendedRequest } from "../types/extended-request";
 import { addTweetSchema } from "../schemas/add-tweet";
+import { findTweet } from "../services/tweet";
 
 export const addTweet = async (req: ExtendedRequest, res: Response) => {
   const safeData = addTweetSchema.safeParse(req.body);
@@ -8,5 +9,14 @@ export const addTweet = async (req: ExtendedRequest, res: Response) => {
   if (!safeData.success) {
     return res.json({ error: safeData.error.flatten().fieldErrors });
   }
+
+  if (safeData.data.answer) {
+    const hasAnswerTweet = await findTweet(parseInt(safeData.data.answer));
+
+    if (!hasAnswerTweet) {
+      return res.json({ error: "Tweet original inexistente" });
+    }
+  }
+
   res.json({});
 };
